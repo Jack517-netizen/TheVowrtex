4734f6da4da5f34043e0fe9568c480721be446fe
JackJosue517 <jackjosue517@gmail.com>
2024-04-07 01:00:04 +0000

    Update for PWA game resolution (512)

--- vite.config.ts
+++ vite.config.ts
@@ -4,25 +4,29 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     VitePWA({
+      injectRegister: 'auto',
       registerType: 'autoUpdate',
       devOptions: {
         enabled: true,
       },
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'maskable-icon.png'],
       manifest: {
         name: 'Vortex',
         short_name: 'Vortex',
-        description: 'Race care game to enjoy',
+        description: 'The Ultimate Racing Universe',
         theme_color: '#000000',
         orientation: 'landscape',
+        display: 'fullscreen',
+        display_override: ['fullscreen'],
+        start_url: '/',
         icons: [
           {
-            src: 'assets/img/android-chrome-192x192.png',
+            src: '/assets/img/android-chrome-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/assets/img/android-chrome-512x512.jpg',
+            src: '/assets/img/android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
