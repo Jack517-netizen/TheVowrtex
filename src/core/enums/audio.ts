2ddec4f30d8e72bd8073fe8f6e607c028278fcef
JackJosue517 <jackjosue517@gmail.com>
2024-04-16 00:09:40 +0000

    Setting up audio architecture and Working on the sound-still-playing-in-background bug

---
+++ src/core/enums/audio.ts
@@ -0,0 +1,26 @@
+/**
+ *  Dividing the types of audio assets
+ * used in VORTEX into three main categories: sound, speech, music
+ */
+export enum AudioType {
+  /**
+   * Any sort of non-verbal sounds that are propagated
+   * from the characters, objects, environment and virtual interfaces in the game
+   *  belong to this group.
+   * Some examples include the hiss of the wind, the rumble of thunder,
+   * footsteps of an approaching enemy, or the bang of a shotgun.
+   */
+  SOUND,
+
+  /**
+   * self-explanatorily refers to the spoken words enacted by voice
+   * actors
+   */
+  SPEECH,
+
+  /**
+   * The intelligent organization of pitch, time, loudness and timbre of
+   * sound results in an aural form of expression which can be differentiated as music
+   */
+  MUSIC,
+}
