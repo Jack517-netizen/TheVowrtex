d0d71c3cb766b7daff3436f52850517f8aa2c9c6
JackJosue517 <jackjosue517@gmail.com>
2024-04-18 01:20:09 +0000

    Refresh home scene look and add icons provided by Flaticon

---
+++ src/core/components/overlay.ts
@@ -0,0 +1,22 @@
+import { Button, Container, Control } from '@babylonjs/gui'
+import { colors } from '../../configs/colors'
+
+export class GameOverlay {
+  /**
+   * createDarkOverlay create a transparent dark overlay
+   * @param void
+   * @returns Container
+   */
+  public static createDarkOverlay(): Container {
+    let _tmp = new Container('overlay-container')
+    _tmp.width = 100
+    _tmp.height = 100
+    _tmp.background = colors.dark.competitive
+    _tmp.alpha = 0.5
+    _tmp.isPointerBlocker = true
+    _tmp.isVisible = true
+
+    return _tmp
+  }
+
+}
