b6c72e56ccbb23a89d7bf34a59648ed3d7e748a3
Jack517-netizen <kodjojosueayitey@gmail.com>
2024-07-30 09:44:53 +0000

    Config environments keys

--- src/core/components/app.ts
+++ src/core/components/app.ts
@@ -3,15 +3,7 @@ import { initializeApp } from 'firebase/app'
 export class APP {
   // Your web app's Firebase configuration (For
   // Firebase JS SDK v7.20.0 and later, measurementId is optional)
-  private static _firebaseConfig: any = {
-    apiKey: 'AIzaSyDqSkPyq2l92R27JFbYyYQmNdYh-UpRcJ4',
-    authDomain: 'vortexx-e89f8.firebaseapp.com',
-    projectId: 'vortexx-e89f8',
-    storageBucket: 'vortexx-e89f8.appspot.com',
-    messagingSenderId: '76885884562',
-    appId: '1:76885884562:web:84ecce2058c3af5094f04b',
-    measurementId: 'G-3JPQNTSWG3',
-  }
+  private static _firebaseConfig: any = {}
 
   // Initialize Firebase
   private static _app = initializeApp(APP._firebaseConfig)
